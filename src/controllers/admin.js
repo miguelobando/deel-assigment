3a631e7237655715474a111186807cc860abd417
Miguel Obando <miguel.obando204@gmail.com>
2023-10-10 00:25:09 -0500

    Add best profesions path

--- src/controllers/admin.js
+++ src/controllers/admin.js
@@ -1,25 +1,50 @@
 
 const { sequelize } = require('../model')
+const { Op } = require('sequelize');
 
-function validateDate(date) {
+function validateFormat(date) {
     const dateRegex = /^\d{4}-\d{2}-\d{2}$/;
     return dateRegex.test(date);
 }
+
+function validateDate(startDate, endDate, res) {
+    let response = 'OK';
+    if (!startDate || !endDate) {
+        response = 'start and end date are required'
+        // return res.status(400)
+        // .json({ message: "start and end date are required" })
+    }
+
+    if (!validateFormat(startDate) || !validateFormat(endDate)) {
+        response = 'start and end date should be in format yyyy-mm-dd'
+        // return res.status(400)
+        // .json({ message: "start and end date should be in format yyyy-mm-dd" })
+    }
+
+    return response;
+
+
+}
+
 
 
 async function getBestProfession(req, res) {
     const startDate = req.query.start;
     const endDate = req.query.end;
 
-    if (!startDate || !endDate) {
-        return res.status(400)
-            .json({ message: "start and end date are required" })
+    const validationResponse = validateDate(startDate, endDate, res)
+    if (validationResponse !== 'OK') {
+        return res.status(400).json({ message: validationResponse })
     }
+    // if (!startDate || !endDate) {
+    //     return res.status(400)
+    //         .json({ message: "start and end date are required" })
+    // }
 
-    if (!validateDate(startDate) || !validateDate(endDate)) {
-        return res.status(400)
-            .json({ message: "start and end date should be in format yyyy-mm-dd" })
-    }
+    // if (!validateFormat(startDate) || !validateFormat(endDate)) {
+    //     return res.status(400)
+    //         .json({ message: "start and end date should be in format yyyy-mm-dd" })
+    // }
 
 
 
@@ -40,7 +65,7 @@ async function getBestProfession(req, res) {
 
     if (!result[0].length) {
         return res.status(404)
-            .json({ message: "No jobs found in that range" })
+            .json({ message: "No professions found in that range" })
     }
 
     const response = {
@@ -53,6 +78,53 @@ async function getBestProfession(req, res) {
 }
 
 
+async function getBestClients(req, res) {
+    const startDate = req.query.start;
+    const endDate = req.query.end;
+
+    if (!startDate || !endDate) {
+        return res.status(400)
+            .json({ message: "start and end date are required" })
+    }
+
+    if (!validateFormat(startDate) || !validateFormat(endDate)) {
+        return res.status(400)
+            .json({ message: "start and end date should be in format yyyy-mm-dd" })
+    }
+
+
+    // I know there is a way to do this with sequelize 
+    // but i found it easier and shorter to do it in this way
+    const query = await sequelize.query(`
+    SELECT p.id, p.firstName || ' ' || p.lastName as fullName, SUM(j.price) as totalPaid
+    FROM Profiles p
+        JOIN Contracts c ON p.id = c.ClientId
+        JOIN Jobs j ON c.id = j.ContractId
+            WHERE j.paid = true
+                AND j.paymentDate >= '${startDate}' 
+                AND j.paymentDate <= '${endDate}' 
+    GROUP BY p.firstName, p.lastName
+    ORDER BY totalPaid DESC
+    LIMIT 2
+    `)
+
+    if (!query[0].length) {
+        return res.status(404)
+            .json({ message: "No clients found in that range" })
+    }
+
+    const result = query[0].map(client => {
+        return {
+            "id": client.id,
+            "fullName": client.fullName,
+            "paid": client.totalPaid
+        }
+    })
+
+    return res.status(200).json(result);
+}
+
 module.exports = {
-    getBestProfession
+    getBestProfession,
+    getBestClients
 }
